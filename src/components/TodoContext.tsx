bd3b5f838c695a3fdb0aded0ccf059d0a1b1d4f3
Fabiola <fabiola.aurelien@gmail.com>
2026-05-04 10:44:12 -0400

    Initialized state of Todo list in TodoProvider function and buiolding interface type as needed.  Currently, only have Todo interface

--- src/components/TodoContext.tsx
+++ src/components/TodoContext.tsx
@@ -1,1 +1,13 @@
+import { useState } from "react";
 import { TodoInput } from "./TodoInput";
+import type { Todo } from "../types";
+import { useState } from "react";
+
+export function TodoProvider({ children }: { children: React.ReactNode }){
+    
+// Initialize the state of the todos to whatever is currently stored in localStorag
+const [todos, setTodos] = useState<Todo[]>(())=>{
+    const 
+}
+
+}
