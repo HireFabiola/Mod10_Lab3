c91c367e25360eb754551ff5a0fa55a6abfc82a5
Fabiola <fabiola.aurelien@gmail.com>
2026-05-04 10:02:53 -0400

    Added button and button handler function in TodoInput and created call to addTodo which isn't yet defined.  Created shell for TodoContext to work on call made from TodoInput

---
+++ src/components/TodoContext.tsx
@@ -0,0 +1,1 @@
+import { TodoInput } from "./TodoInput";
--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 
-export function TodoInput(){
+
+export function TodoInput() {
 
     // Inititalize input to empty string
-    const [input, setInput] = useState("";)
+    const [input, setInput] = useState("");
 
+    // Declared function 
+    function handleAddTodo(){
+
+        // Call addTodo to addd inputed Todo item
+        handleAddTodo(){
+        addTodo("input");
+
+        // Clear out input field after
+        setInput("");
+        }
+    }
 
     // Create input field with listener
-    return(
+    return (
         <div>
-            <input type = "text" placeholder = "Add a todo..." value={input} onChange={(e)=>setInput(e.target.value)}/>
+            <input type="text" placeholder="Add a todo..." value={input} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={handleAddTodo}>Add Todo</button>
+            <p>Input: {input}</p>
         </div>
+
     )
+
 }
