14a7cea3ee23caea5544a634fa276506643899cb
Fabiola <fabiola.aurelien@gmail.com>
2026-05-12 05:54:36 -0400

    Added Todo Provider function

--- src/components/TodoContext.tsx
+++ src/components/TodoContext.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoInput } from "./TodoInput";
 import type { Todo } from "../types";
 import { useState } from "react";
 
 export function TodoProvider({ children }: { children: React.ReactNode }){
-    
-// Initialize the state of the todos to whatever is currently stored in localStorag
-const [todos, setTodos] = useState<Todo[]>(())=>{
-    const 
-}
+ 
+// Initialize the state of the todos to whatever is currently stored in localStorage
+const [todos, setTodos] = useState<Todo[]>(()=>{
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+});
 
-}
+// Trigger setting item in localStoarge when state of todos changes
+useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}, [todos]);
+
+
