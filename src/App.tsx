a09d6610950eb67701988a1ee78e04409c2bfb10
Fabiola Aurelien <fabiola.aurelien@gmail.com>
2026-05-16 13:25:54 -0400

    Moved Provider wrapping to main.tsx from App.tsx to give App access to context hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import './App.css'
 import { TodoInput } from './components/TodoInput'
-import { TodoProvider } from './components/TodoContext'
 import { TodoList } from './components/TodoList'
 
 
 function App() {
   return (
     <div className="listContainer">
-      < TodoProvider >
-        <TodoInput />
-        <TodoList />
-      </TodoProvider >
+      <TodoInput />
+      <TodoList />
     </div >
   )
 }
