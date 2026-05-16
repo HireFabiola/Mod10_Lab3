20606bdb17f96afa204191c429d5547313542c2e
Fabiola Aurelien <fabiola.aurelien@gmail.com>
2026-05-16 14:49:12 -0400

    Added filter logic to TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,39 @@
 import { useTodos } from "./TodoContext";
+import { useFilter } from "./FilterContext";
 import TodoItem from "./TodoItem";
 
 // Function call to display todo list
 export function TodoList() {
+
+  // Access todos from TodoContext
   const { todos } = useTodos();
+
+  // Access current visibility filter from FilterContext
+  const { filter } = useFilter();
+
+  // Filter todos based on current filter value
+  const filteredTodos = todos.filter((todo) => {
+
+    // Show only active todos
+    if (filter === "active") {
+      return !todo.completed;
+    }
+
+    // Show only completed todos
+    if (filter === "completed") {
+      return todo.completed;
+    }
+
+    // Default: show all todos
+    return true;
+  });
 
   return (
     <div>
       <h2>Todo List</h2>
 
-      {todos.map((todo) => (
+      {/* Render filtered todo list */}
+      {filteredTodos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} />
       ))}
     </div>
