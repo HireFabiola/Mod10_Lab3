c6c685663fad5c11ecd5550df7c3974a353a3138
Fabiola Aurelien <fabiola.aurelien@gmail.com>
2026-05-16 14:33:47 -0400

    Wrapped App in FilterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { TodoProvider } from './components/TodoContext.tsx';
+import { FilterProvider } from './components/FilterContext.tsx';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-<TodoProvider>
-  <App />
-</TodoProvider>
+    <FilterProvider>
+      <TodoProvider>
+        <App />
+      </TodoProvider>
+    </FilterProvider>
   </StrictMode>,
 )
