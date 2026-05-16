6636d99d1fa143d832d56909c67e25e52ee921a1
Fabiola Aurelien <fabiola.aurelien@gmail.com>
2026-05-16 15:33:03 -0400

    Added ThemeContext file and code and wrapped main in ThemeProvider

---
+++ src/components/ThemeContext.tsx
@@ -0,0 +1,39 @@
+// src/components/ThemeContext.tsx
+
+import { createContext, useContext, useState } from "react";
+import type { ReactNode } from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  function toggleTheme() {
+    setTheme((prevTheme) =>
+      prevTheme === "light" ? "dark" : "light"
+    );
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+
+  return context;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { TodoProvider } from './components/TodoContext.tsx';
 import { FilterProvider } from './components/FilterContext.tsx';
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './components/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider
     <FilterProvider>
       <TodoProvider>
         <App />
       </TodoProvider>
     </FilterProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
