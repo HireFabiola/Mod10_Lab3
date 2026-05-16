f69705f6f6ff9489128be2408e03944500fbd448
Fabiola Aurelien <fabiola.aurelien@gmail.com>
2026-05-16 15:51:03 -0400

    Found a pesky comma that was crashing the whole output

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TodoProvider } from './components/TodoContext.tsx';
-import { FilterProvider } from './components/FilterContext.tsx';
+
+import { TodoProvider } from './components/TodoContext.tsx'
+import { FilterProvider } from './components/FilterContext.tsx'
+import { ThemeProvider } from './components/ThemeContext.tsx'
+
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider } from './components/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider
-    <FilterProvider>
+    <ThemeProvider>
       <TodoProvider>
-        <App />
+        <FilterProvider>
+          <App />
+        </FilterProvider>
       </TodoProvider>
-    </FilterProvider>
     </ThemeProvider>
   </StrictMode>,
 )
