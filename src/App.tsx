73d3f536733e4b28d8c43f5b80773c37f4c6a7dc
Fabiola Aurelien <fabiola.aurelien@gmail.com>
2026-05-16 13:31:15 -0400

    Added clearCompleted button to main App component.  Will be active or deactive based on presence of an active list

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,35 @@ import './App.css'
 import { TodoInput } from './components/TodoInput'
 import { TodoList } from './components/TodoList'
 
+// Import custom context hook
+import { useTodos } from './context/TodoContext'
 
 function App() {
+
+  // Access todos state and clearCompleted function from context
+  const { todos, clearCompleted } = useTodos()
+
+  // Check whether any completed todos exist
+  // Used to enable/disable the button dynamically
+  const hasCompletedTodos = todos.some(
+    (todo) => todo.completed
+  )
+
   return (
     <div className="listContainer">
       <TodoInput />
       <TodoList />
-    </div >
+
+      {/* Button to remove all completed todos */}
+      <button
+        onClick={clearCompleted}
+        disabled={!hasCompletedTodos}
+      >
+        Clear Completed
+      </button>
+
+    </div>
   )
 }
+
 export default App
